ff13b0d87e64fb2f4964987bc5f555617592a491
Jorgehadad <jorgemenegaz@gmail.com>
2022-01-12 00:20:41 -0300

    add housed entity and visitors

--- park-app/src/main/java/com/solvd/jdbc/RunnerJDBC.java
+++ park-app/src/main/java/com/solvd/jdbc/RunnerJDBC.java
@@ -4,8 +4,12 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import com.solvd.jdbc.connections.PoolConnection;
+import com.solvd.jdbc.dao.HousedDAO;
 import com.solvd.jdbc.dao.ParkDAO;
+import com.solvd.jdbc.dao.VisitorDAO;
+import com.solvd.jdbc.entities.Housed;
 import com.solvd.jdbc.entities.Park;
+import com.solvd.jdbc.entities.Visitor;
 
 public class RunnerJDBC {
 
@@ -21,6 +25,8 @@ public class RunnerJDBC {
             System.out.println(conn + "Connection failed");
         }
 
+//------------------------------------------------------------------------//
+        /*
         Park park = new Park();
         park.setNameP("With Summer2");
         park.setDateDeclaracion(java.sql.Date.valueOf("2021-01-01"));
@@ -31,7 +37,35 @@ public class RunnerJDBC {
         //ParksService.getByNameA(area.getNameA());
         //ParksService.update(area);
         //ParksService.delete(area);
+        */
+ //------------------------------------------------------------------------//
+        /*
+        Housed housed = new Housed();
+        housed.setNumAccomodation(1);
+        housed.setDniVisitor(1);
+        housed.setarrivalDate(java.sql.Date.valueOf("2021-01-01"));
+        housed.setdepartureDate(java.sql.Date.valueOf("2021-01-01"));
+        housed.setRoom("1");
+
+        HousedDAO housedDAO = new HousedDAO();
+        //housedDAO.save(housed); //foreing key problems safe update
+        housedDAO.getAllLimit10();
+        */
+ //------------------------------------------------------------------------//
+        /*
+        Visitor visitor = new Visitor();
+        visitor.setDNI(1);
+        visitor.setName("Vasya");
+        visitor.setAddress("Street 1");
+        visitor.setProfession("Student");
+
+        VisitorDAO visitorDAO = new VisitorDAO();
+        //visitorDAO.save(visitor);
+        visitorDAO.getAll();
+        */
+//------------------------------------------------------------------------//
         
+
 
     }
     
