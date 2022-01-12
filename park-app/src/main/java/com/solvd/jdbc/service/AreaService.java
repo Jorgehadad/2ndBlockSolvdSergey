27fa2f92b97ce252f15a41128b081d0d0dab5727
Jorgehadad <jorgemenegaz@gmail.com>
2022-01-12 01:30:37 -0300

    areaService

--- park-app/src/main/java/com/solvd/jdbc/service/AreaService.java
+++ park-app/src/main/java/com/solvd/jdbc/service/AreaService.java
@@ -1,5 +1,37 @@
 package com.solvd.jdbc.service;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.solvd.jdbc.dao.AreaDAO;
+import com.solvd.jdbc.entities.Area;
+
 public class AreaService {
-        
+    private static AreaDAO areaDAO;
+
+    public AreaService() {
+        areaDAO = new AreaDAO();
+    }
+
+    public void save(Area area) throws SQLException {
+        areaDAO.save(area);
+    }
+
+    public void update(Area area) throws SQLException {
+        areaDAO.update(area);
+    }
+
+    public void delete(Area area) throws SQLException {
+        areaDAO.delete(area);
+    }
+
+    //getByNameP
+    public Area getByNameP(String NameP) throws SQLException {
+        return areaDAO.getByNameP(NameP);
+    }
+
+    //getAllByNameP
+    public List<Area> getAllByNameP(String NameP) throws SQLException {
+        return areaDAO.getAllByNameP(NameP);
+    }
 }
