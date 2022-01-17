1b419f5a155e5c6c8f8031d0e590b59435778ad6
Jorgehadad <52721467+Jorgehadad@users.noreply.github.com>
2022-01-16 23:07:57 -0300

    comments in the builder pattern

--- park-app/src/main/java/com/solvd/builder/BuilderPattern.java
+++ park-app/src/main/java/com/solvd/builder/BuilderPattern.java
@@ -5,5 +5,8 @@ public class BuilderPattern {
     // complex objects step by step.
     // The Builder pattern is used to construct complex objects with a step-by-step
     // approach.
-
+    
+    //follow this example:
+    //https://www.tutorialspoint.com/design_pattern/builder_pattern.htm
+    //https://howtodoinjava.com/design-patterns/creational/builder-pattern-in-java/
 }
