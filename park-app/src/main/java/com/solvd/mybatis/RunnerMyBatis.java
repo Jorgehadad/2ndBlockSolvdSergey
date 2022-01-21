1cccc268449ec089b24712cee0fd32884ebd740d
Jorgehadad <jorgemenegaz@gmail.com>
2022-01-20 21:19:07 -0300

    merge every runner mybatis class into one

---
+++ park-app/src/main/java/com/solvd/mybatis/RunnerMyBatis.java
@@ -0,0 +1,95 @@
+package com.solvd.mybatis;
+
+import java.sql.Date;
+
+import com.solvd.entities.Park;
+import com.solvd.entities.Staff;
+import com.solvd.entities.Visitor;
+import com.solvd.mybatis.connection.ConnectionBuilder;
+import com.solvd.mybatis.mapper.AreaMapperMyBatis;
+import com.solvd.mybatis.mapper.HousedMapperMyBatis;
+import com.solvd.mybatis.mapper.ParkMapperMyBatis;
+import com.solvd.mybatis.mapper.StaffMapperMyBatis;
+import com.solvd.mybatis.mapper.VisitorMapperMyBatis;
+
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+
+
+public class RunnerMyBatis {
+
+    public static void main(String[] args) {
+        try (SqlSession session = new ConnectionBuilder().buildConnection()) {
+            System.out.println(session.getMapper(ParkMapperMyBatis.class).getParkByName("With Summer"));
+
+            //update
+            Park park = new Park();
+            park.setDateDeclaracion(Date.valueOf("2022-01-01"));
+            park.setNameP("With Summer");
+            //session.getMapper(ParkMapperMyBatis.class).updatePark(park);
+            //new SQL DATE
+
+
+            //insert
+            park.setNameP("With Winter");
+            park.setDateDeclaracion(Date.valueOf("2022-01-01"));
+            //session.getMapper(ParkMapperMyBatis.class).insertPark(park);
+
+            session.commit();
+            session.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // --------------------------------------------------
+        
+        try (SqlSession session = new ConnectionBuilder().buildConnection()) {
+            System.out.println(session.getMapper(AreaMapperMyBatis.class).getAreaByName("Area 21"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // --------------------------------------------------
+
+        try (SqlSession session = new ConnectionBuilder().buildConnection()) {
+            System.out.println(session.getMapper(HousedMapperMyBatis.class).getAllHoused());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // --------------------------------------------------
+
+
+        try (SqlSession session = new ConnectionBuilder().buildConnection()) {
+            for (Staff staff : session.getMapper(StaffMapperMyBatis.class).getAll()) {
+                System.out.println(staff + "\n");
+            }
+            //System.out.println(session.getMapper(StaffMapperMyBatis.class).getAll().iterator().next());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // --------------------------------------------------
+
+        try (SqlSession session = new ConnectionBuilder().buildConnection()) {
+            for (Visitor visitor : session.getMapper(VisitorMapperMyBatis.class).getAll()) {
+                System.out.println(visitor + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+}
--- park-app/src/main/java/com/solvd/mybatis/service/PakServiceMyBatis.java
+++ park-app/src/main/java/com/solvd/mybatis/service/PakServiceMyBatis.java
@@ -1,5 +1,41 @@
 package com.solvd.mybatis.service;
 
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.solvd.entities.Park;
+import com.solvd.mybatis.mapper.ParkMapperMyBatis;
+
 public class PakServiceMyBatis {
+    private static ParkMapperMyBatis parkMapper;
     
+    public void insertPark(Park park) throws SQLException {
+        parkMapper.insertPark(park);
+    }
+
+    //getParkByName
+    public Park getParkByName(String nameP) throws SQLException {
+        return parkMapper.getParkByName(nameP);
+    }
+
+    //updatePark
+    public void updatePark(Park park) throws SQLException {
+        parkMapper.updatePark(park);
+    }
+
+    //deletePark
+    public void deletePark(String nameP) throws SQLException {
+        parkMapper.deletePark(nameP);
+    }
+
+    //getAllParks
+    public List<Park> getAllParks() throws SQLException {
+        return parkMapper.getAllParks();
+    }
+
+    //getParkByDate
+    public Park getParkByDate(Date date) throws SQLException {
+        return parkMapper.getParkByDate(date);
+    }
 }
